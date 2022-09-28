0b9148ae53a1bec13c98f1d505597218e98b78ee
hirokazu kobayashi <solness.stf@gmail.com>
2022-09-28 20:42:59 +0900

    Introduce temporary directory

--- hello/urls.py
+++ hello/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('upload', views.model_form_upload, name='upload'),
     path('file_download', views.file_download, name='file_download'),
+    path('download_complete', views.download_complete, name='download_complete'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -5,59 +5,73 @@ import cv2
 from django.conf import settings
 from django.utils import timezone
 import glob
-from django.http import FileResponse
+from django.http import FileResponse, HttpResponse
 import os
+from tempfile import TemporaryDirectory
 
 def model_form_upload(request):
-    print('①こんにちは！')
+
+
     if request.method == 'POST':
-        print('②こんにちは！')
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-        photo = Document.objects.order_by("id").last()
-        url = photo.photo
-        picture_type = photo.picture_type
-        if picture_type == 'black_white':
-            gray(url)
-        elif picture_type == 'mosaic':
-            mosaic(url)
-        elif picture_type == 'sepia':
-            sepia(url)
-        photo.delete()
-        print('③こんにちは！')
+        # photo = Document.objects.order_by("id").last()
+        # url = photo.photo
+        # picture_type = photo.picture_type
+        # if picture_type == 'black_white':
+        #     gray(url)
+        # elif picture_type == 'mosaic':
+        #     mosaic(url)
+        # elif picture_type == 'sepia':
+        #     sepia(url)
+        # photo.delete()
         return redirect('file_download')
+
     else:
         form = DocumentForm()
-
-    # remove file that is already downloaded
-    altered_pic_dir = os.listdir('./media/gallery/')
-    if len(altered_pic_dir) != 0:
-        print('④こんにちは！！')
-        print(altered_pic_dir)
-        for f in altered_pic_dir:
-            os.remove(os.path.join('./media/gallery/', f))
-
-    #print('おばんです！！')
-
     return render(request, 'hello/model_form_upload.html', {
         'form': form
     })
 
 #download files
 def file_download(request):
-    print('⑤こんにちは！！')
-    file_path = glob.glob('./media/gallery/*')
-    file_name = os.path.basename(file_path[0])
-    return FileResponse(open(file_path[0], "rb"), as_attachment=True, filename=file_name)
+    with TemporaryDirectory() as tempdir:
+        photo = Document.objects.order_by("id").last()
+        url = photo.photo
+        picture_type = photo.picture_type
+        path = settings.MEDIA_ROOT + "/" + str(url)
+        img = cv2.imread(path)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # output = settings.MEDIA_ROOT + "/gallery/GRAY)" + str(timezone.now()) + ".jpg"
+        output = tempdir + str(timezone.now()) + ".jpg"
+        cv2.imwrite(output, img_gray)
+        photo.delete()
+        file_name = os.path.basename(output)
+        return FileResponse(open(output, "rb"), as_attachment=True, filename=file_name)
+
+# remove file that is already downloaded
+def download_complete(request):
+    altered_pic_dir = './media/gallery/'
+    altered_pic = os.listdir(altered_pic_dir)
+    if len(altered_pic) != 0:
+        for f in altered_pic:
+            os.remove(os.path.join(altered_pic_dir, f))
+    return render(request, 'hello/model_form_upload.html')
 
 # convert into gray-color
-def gray(url):
+def gray(request):
+    photo = Document.objects.order_by("id").last()
+    url = photo.photo
+    picture_type = photo.picture_type
     path = settings.MEDIA_ROOT + "/" + str(url)
     img = cv2.imread(path)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     output = settings.MEDIA_ROOT + "/gallery/GRAY)" + str(timezone.now()) + ".jpg"
     cv2.imwrite(output, img_gray)
+    file_path = glob.glob('./media/gallery/*')
+    file_name = os.path.basename(file_path[0])
+    return FileResponse(open(file_path[0], "rb"), as_attachment=True, filename=file_name)
 
 # convert into mosaic
 def mosaic(url):
