612ac85ce27b3548b8a3a38c4740d0f628159d1b
hirokazu kobayashi <solness.stf@gmail.com>
2022-10-04 21:47:17 +0900

    delete processed pictures in the folder if the number exceeds 10

--- hello/views.py
+++ hello/views.py
@@ -14,11 +14,11 @@ import datetime
 
 def model_form_upload(request):
 
-    # DIR = settings.MEDIA_ROOT + "/gallery/"
-    # pic_amount = sum(os.path.isfile(os.path.join(DIR, name)) for name in os.listdir(DIR))
-    # if pic_amount > 30:
-    #     for file in os.scandir(DIR):
-    #         os.remove(file.path)     
+    DIR = settings.MEDIA_ROOT + "/gallery/"
+    pic_amount = sum(os.path.isfile(os.path.join(DIR, name)) for name in os.listdir(DIR))
+    if pic_amount > 10:
+        for file in os.scandir(DIR):
+            os.remove(file.path)     
 
 
     if request.method == 'POST':
@@ -116,53 +116,3 @@ def sepia(img):
     img[:,:,(1)] = img[:,:,(1)] * 0.8
     img[:,:,(2)] = img[:,:,(2)]
     return img
-
-
-
-
-
-
-
-
-
-#download files
-# def gray(request):
-#     with TemporaryDirectory() as tempdir:
-#         photo = Document.objects.order_by("id").last()
-#         url = photo.photo
-#         path = settings.MEDIA_ROOT + "/" + str(url)
-#         img = cv2.imread(path)
-#         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#         output = tempdir + "BLACKWHITE】" + str(timezone.now()) + ".jpg"
-#         cv2.imwrite(output, img_gray)
-#         photo.delete()
-#         file_name = os.path.basename(output)
-#         return FileResponse(open(output, "rb"), as_attachment=True, filename=file_name)
-
-# convert into mosaic
-# def mosaic(request):
-#     with TemporaryDirectory() as tempdir:
-#         photo = Document.objects.order_by("id").last()
-#         url = photo.photo
-#         path = settings.MEDIA_ROOT + "/" + str(url)
-#         img = cv2.imread(path)
-#         small = cv2.resize(img, None, fx=0.1, fy=0.1)
-#         img_mosaic = cv2.resize(small, img.shape[:2][::-1])
-#         output = tempdir + "MOSAIC】" + str(timezone.now()) + ".jpg"
-#         cv2.imwrite(output, img_mosaic)
-#         photo.delete()
-#         file_name = os.path.basename(output)
-# def sepia(request):
-#     with TemporaryDirectory() as tempdir:
-#         photo = Document.objects.order_by("id").last()
-#         url = photo.photo
-#         path = settings.MEDIA_ROOT + "/" + str(url)
-#         img = cv2.imread(path)
-#         img[:,:,(0)] = img[:,:,(0)] * 0.3
-#         img[:,:,(1)] = img[:,:,(1)] * 0.8
-#         img[:,:,(2)] = img[:,:,(2)]
-#         output = tempdir + "SEPIA】" + str(timezone.now()) + ".jpg"
-#         cv2.imwrite(output,img) 
-#         photo.delete()
-#         file_name = os.path.basename(output)
-#         return FileResponse(open(output, "rb"), as_attachment=True, filename=file_name)
